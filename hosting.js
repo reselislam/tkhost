4d5294469bff4b1665946bab7efacb2573de4f1e
reselislam <95220079+reselislam@users.noreply.github.com>
2022-05-15 14:04:55 +0600

    Add files via upload

---
+++ hosting.js
@@ -0,0 +1,24 @@
+const api = require("discord.js");
+const fs = require('fs');
+
+const data = fs.readFileSync('tokens.txt', 'UTF-8');
+const lines = data.split(/\r?\n/);
+
+lines.forEach((line) => {
+    Hosting(line);
+});
+
+function Hosting(token)
+{
+    const userclient = new api.Client()
+    userclient.on("ready", () => {
+        userclient.user.setActivity("RƛGӇƛV OP"); 
+        console.log('Connected: ' + userclient.user.tag);
+    });
+    userclient.login(token);
+}
+
+process.on('uncaughtException', function (exception) {
+});
+process.on('unhandledRejection', error => {
+});
